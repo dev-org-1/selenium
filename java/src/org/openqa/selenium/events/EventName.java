c64b28e3562e7b8dcbfdf8b6792894c0ce4bc421
risknav <137483023+risknav@users.noreply.github.com>
2023-12-21 15:47:15 +0530

    Update EventName.java

--- java/src/org/openqa/selenium/events/EventName.java
+++ java/src/org/openqa/selenium/events/EventName.java
@@ -14,7 +14,42 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.hibernate.cfg.Configuration;
+ 
+public class Main {
+    public static void main(String[] args) {
+        // Assume inputColumnName is obtained from user input
+        String inputColumnName = "age"; // Assume a valid column name
+ 
+        // Create a Hibernate configuration
+        Configuration configuration = new Configuration().configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+ 
+        // Open a session
+        try (Session session = sessionFactory.openSession()) {
+            // Begin a transaction
+            Transaction transaction = session.beginTransaction();
+ 
+            // The modified code using a parameterized query
+            String hsql = "update People set " + inputColumnName + " = :value";
+            Query query = session.createQuery(hsql);
+            query.setParameter("value", null);
+            query.executeUpdate();
+ 
+            // Commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the session factory
+            sessionFactory.close();
+        }
+    }
+}
 package org.openqa.selenium.events;
 
 import java.util.Objects;
